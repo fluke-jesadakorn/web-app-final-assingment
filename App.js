01a797bd2b8cb4b71b1f8fddb3ecbdfe2fc17972
Fluke <jesadakorn.kirtnu@gmail.com>
2019-11-30 03:31:59 +0700

    fetchAndAddListJson

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ApiChart from './component/cryptochart'
+import { dataList } from './component/json'
 
 export default App = () => {
+  const [fetchJson, getFetchJson] = useState([]);
+  console.log(dataList)
   return (
     <ApiChart />
   )
---
+++ component/json.js
@@ -0,0 +1,62 @@
+export const dataList = [
+    {
+      id:1,
+      symbol: "BTC",
+      name:"bitcoin",
+      logo: "https://www.cryptocompare.com/media/19633/btc.png",
+    },
+    {
+      id:2,
+      symbol: "ETH",
+      name:"etherium",
+      logo: "https://www.cryptocompare.com/media/20646/eth_logo.png",
+    },
+    {
+      id:3,
+      symbol: "BCH",
+      name:"bitcoin cash",
+      logo: "https://www.cryptocompare.com/media/35650680/bch.png",
+    },
+    {
+      id:4,
+      symbol: "XRP",
+      name:"ripple",
+      logo: "https://www.cryptocompare.com/media/34477776/xrp.png",
+    },
+    {
+      id:5,
+      symbol: "ZEC",
+      name:"ZCash",
+      logo: "https://www.cryptocompare.com/media/351360/zec.png",
+    },
+    {
+      id:6,
+      symbol:"LTC",
+      name:"Litecoin",
+      logo:"https://www.cryptocompare.com/media/35309662/ltc.png",
+    },
+    {
+      id:7,
+      symbol:"DOGE",
+      name:"Dogecoin",
+      logo:"https://www.cryptocompare.com/media/19684/doge.png",
+    },
+    {
+      id:8,
+      symbol:"OMG",
+      name:"OmiseGO",
+      logo:"https://www.cryptocompare.com/media/1383814/omisego.png",
+    },
+    {
+      id:9,
+      symbol:"EVX",
+      name:"Everex",
+      logo:"https://www.cryptocompare.com/media/1383850/evx.png",
+    },
+    {
+      id:10,
+      symbol:"BNB",
+      name:"Binance coin",
+      logo:"https://www.cryptocompare.com/media/1383947/bnb.png",
+    },
+  ];
